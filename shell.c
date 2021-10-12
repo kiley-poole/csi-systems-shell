8ecf47b66de417584a4a1f157e6b3c878a3654ba
Kiley Poole <kpoole007@gmail.com>
2021-10-12 16:19:48 -0700

    init commit

---
+++ shell.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <errno.h>
+
+
+#include <unistd.h>
+#define MAXLINE 1000
+#define MAXARGS 5
+
+int main(){
+
+    char cmd[MAXLINE];
+    char *argv[]={"ls",NULL};
+    int cmdErr = 0;
+    pid_t pid;
+
+    while(1){
+        printf("@ ");
+        fgets(cmd, MAXLINE, stdin);
+        if (feof(stdin)){
+            printf("\n**SEE YA LATER SUCKER**\n");
+            exit(0);
+        }
+        if((pid = fork()) == 0){
+            if(execvp("cd", argv) < 0){
+                printf("Unknown Command\n");
+                exit(0);
+            }
+        }
+        int status;
+        if (waitpid(pid, &status, 0) < 0){
+            fprintf(stderr, "%s: %s\n", "pid fucked", strerror(errno));
+        }
+    }
+
+}
